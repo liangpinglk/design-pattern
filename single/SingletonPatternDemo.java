169b244e9cad61ef9f985352c782a1ba1de654a8
liangpinglk <liangpinglk@163.com>
2018-03-13 21:24:10 +0800

    单例模式

---
+++ single/SingletonPatternDemo.java
@@ -0,0 +1,10 @@
+package single;
+
+public class SingletonPatternDemo {
+    public static void main(String [] args){
+        SingleObject object = SingleObject.getInstance();
+
+        object.showMessage();
+
+    }
+}
